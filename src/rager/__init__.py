79d56345736932971936cabe7d569556666e6ed0
Wannes Vantorre <vantorrewannes@gmail.com>
2026-07-11 15:58:29 +0200

    Export MarkdownFileParser and PdfFileParser
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>

--- src/rager/__init__.py
+++ src/rager/__init__.py
@@ -4,7 +4,11 @@ from rager.chunkers import Chunker
 from rager.embedders import Embedder
 from rager.generators import Generator
 from rager.indexes import Index
-from rager.parsers import Parser
+from rager.parsers import (
+    MarkdownFileParser,
+    Parser,
+    PdfFileParser,
+)
 from rager.scorers import Scorer
 from rager.stores import Store
 from rager.types import Embedding, Hash
@@ -16,8 +20,9 @@ __all__ = [
     "Generator",
     "Hash",
     "Index",
+    "MarkdownFileParser",
     "Parser",
-    "PdfParser",
+    "PdfFileParser",
     "Scorer",
     "Store",
 ]
