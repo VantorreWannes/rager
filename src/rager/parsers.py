c29869ff2176e317025ee46bdc02cb62ff0c8722
Wannes Vantorre <109341383+VantorreWannes@users.noreply.github.com>
2026-07-11 11:01:27 +0200

    Replace ContentParser with generic TextParser (#14)
    The new TextParser protocol accepts a generic data type `D` for its
    methods, replacing the stateful ContentParser interface. The `content`
    method was also removed as it is no longer needed.
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>

---
+++ src/rager/parsers.py
@@ -0,0 +1,18 @@
+"""Text parsers for extracting units from data."""
+
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from rager.types import Id
+
+
+class TextParser[D](Protocol):
+    """Protocol for text parsers."""
+
+    def units(self, data: D) -> list[str]:
+        """Return the extracted text units."""
+        ...
+
+    def id(self, data: D) -> Id:
+        """Return the content ID."""
+        ...
