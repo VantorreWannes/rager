380f28c2619db4f214946265361b16e4e9f68a8f
Wannes Vantorre <109341383+VantorreWannes@users.noreply.github.com>
2026-07-11 17:13:51 +0200

    Feature/refactor store protocol 34 (#35)
    * Add pre-push hook for integration tests
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>
    
    * Change Store.add to accept key and return None
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>
    
    ---------
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>

--- src/rager/stores.py
+++ src/rager/stores.py
@@ -6,8 +6,8 @@ from typing import Protocol
 class Store[V, K](Protocol):
     """Protocol for a key-value store mapping keys of type K to values of type V."""
 
-    async def add(self, value: V) -> K:
-        """Store a single value and return its key."""
+    async def add(self, key: K, value: V) -> None:
+        """Store a value with the given key."""
         ...
 
     async def get(self, key: K) -> V | None:
