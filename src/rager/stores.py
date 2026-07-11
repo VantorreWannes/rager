c63a539fc6bf8a1e65701791600f6c761e4131e5
Wannes Vantorre <109341383+VantorreWannes@users.noreply.github.com>
2026-07-11 13:31:10 +0200

    Feature/define index protocol (#21)
    * Make Embedder protocol generic
    
    This allows embedders to return different embedding types
    rather than being tied to a single Embedding type.
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>
    
    * Make Store protocol methods async
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>
    
    * Add embedding index protocol
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>
    
    ---------
    
    Signed-off-by: Wannes Vantorre <vantorrewannes@gmail.com>

--- src/rager/stores.py
+++ src/rager/stores.py
@@ -6,10 +6,10 @@ from typing import Protocol
 class Store[V, K](Protocol):
     """Protocol for a key-value store mapping keys of type K to values of type V."""
 
-    def add(self, value: V) -> K:
+    async def add(self, value: V) -> K:
         """Store a single value and return its key."""
         ...
 
-    def get(self, key: K) -> V | None:
+    async def get(self, key: K) -> V | None:
         """Retrieve a value by its key."""
         ...
